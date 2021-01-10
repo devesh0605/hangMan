eed4fc88c7efa70a7d8f17bccadd18008dc8d13a
devesh0605 <dsaxena_be18@thapar.edu>
2021-01-10 13:14:33 +0530

    hangman component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
+import Hangman from './Components/Hangman';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hangman/>
     </div>
   );
 }
